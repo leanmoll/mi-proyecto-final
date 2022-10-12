3c0dcf9de3f6fbe735304500003df50669fb6453
Leandro Moll <leanmoll24@gmail.com>
2022-10-11 23:26:49 -0300

    agrego ejemplo de clase_18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Leandro"})
+
+def imc (request, peso, altura):
+    imc = 1 
+    return render(request, "ejemplo/imc.html", {"imc": imc})
